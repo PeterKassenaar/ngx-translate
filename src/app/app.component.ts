91097cb75969ebbfc85e4914c2eef52d859e59d3
PeterKassenaar <info@kassenaar.com>
2025-05-20 08:58:47 +0200

    fix: update application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,14 @@ export class AppComponent {
   title: string = 'ngx-translate-demo';
   url: string = 'https://www.codeandweb.com/babeledit';
 
+  // Using Constructor Injection here. In newer Angular applications
+  // we can also use a syntax like 'translate = inject(TranslateService)'
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('nl');
     translate.use('nl')
   }
 
+  // Switch language
   useLanguage(language: string): void {
     this.translate.use(language)
   }
